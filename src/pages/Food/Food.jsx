e4dbe30220adc5eb3bdc8b63845a5f0e9d0af4aa
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2023-08-20 20:08:18 +0500

    Add react-skeleton

--- src/pages/Food/Food.jsx
+++ src/pages/Food/Food.jsx
@@ -7,6 +7,7 @@ import Sidebar from '../../components/Sidebar';
 import Header from '../../components/Header';
 import { useState } from 'react';
 import { FoodModal } from '../../components/Modal/foodModal';
+import React_Skeleton from '../../components/React_Skeleton/React_Skeleton';
 
 export const Food = () => {
 	const [addModal, addSetModal] = useState(false);
@@ -81,50 +82,74 @@ export const Food = () => {
 									</tr>
 								</thead>
 								<tbody className=''>
-									{obj.map((item) => (
-										<tr key={item.id} className='table-borderless'>
-											<th scope='row' className='jg text-center'>
-												{item.id}
-											</th>
-											<td scope='row' className='jg text-center'>
-												<Link
-													to={'#'}
-													className='Item-link text-decoration-none text-dark'
+									{obj.length ? (
+										obj.map((item) => (
+											<tr key={item.id} className='table-borderless'>
+												<th scope='row' className='jg text-center'>
+													{item.id}
+												</th>
+												<td scope='row' className='jg text-center'>
+													<Link
+														to={'#'}
+														className='Item-link text-decoration-none text-dark'
+													>
+														{item.name}
+													</Link>
+												</td>
+												<td className='jg text-center'>{item.desc}</td>
+												<td className='jg text-center'>{item.price}</td>
+												<td
+													className='jg text-center'
+													style={
+														item.isActive === 'enabled'
+															? {
+																	backgroundColor: '#D9FFDA',
+																	color: '#008C06',
+																	border: 'transparent',
+															  }
+															: {
+																	backgroundColor: '#FFD9D9',
+																	color: '#BE0707',
+																	border: 'transparent',
+															  }
+													}
 												>
-													{item.name}
-												</Link>
+													{item.isActive}
+												</td>
+												<td className='jg text-center'>
+													{' '}
+													<button
+														className='btn'
+														onClick={() => editSetModal(true)}
+													>
+														<GrEdit color='green' size={20} />
+													</button>
+												</td>
+												<td></td>
+											</tr>
+										))
+									) : (
+										<tr>
+											<td>
+												<React_Skeleton />
 											</td>
-											<td className='jg text-center'>{item.desc}</td>
-											<td className='jg text-center'>{item.price}</td>
-											<td
-												className='jg text-center'
-												style={
-													item.isActive === 'enabled'
-														? {
-																backgroundColor: '#D9FFDA',
-																color: '#008C06',
-																border: 'transparent',
-														  }
-														: {
-																backgroundColor: '#FFD9D9',
-																color: '#BE0707',
-																border: 'transparent',
-														  }
-												}
-											>
-												{item.isActive}
+											<td>
+												<React_Skeleton />
 											</td>
-											<td className='jg text-center'>
-												{' '}
-												<button
-													className='btn'
-													onClick={() => editSetModal(true)}
-												>
-													<GrEdit color='green' size={20} />
-												</button>
+											<td>
+												<React_Skeleton />
+											</td>
+											<td>
+												<React_Skeleton />
+											</td>
+											<td>
+												<React_Skeleton />
+											</td>
+											<td>
+												<React_Skeleton />
 											</td>
 										</tr>
-									))}
+									)}
 								</tbody>
 							</table>
 							<FoodModal
