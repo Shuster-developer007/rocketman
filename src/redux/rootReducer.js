fadd4c61bc1015a926b0538c45efe9681d934f4c
XudoyberdiyevDilshod <1998xudoyberdiev@gmail.com>
2023-08-25 20:10:23 +0500

    Add reduxToken

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,12 @@ import { categoryReducer } from './category/categoryReducer';
 import { subCategoryReducer } from './subCategory/subCategoryReducer';
 import { productReducer } from './product/productReducer';
 import { subProductReducer } from './subProduct/subProductReducer';
+import { tokenReducer } from './token/tokenReducer';
 
 export const rootReducer = combineReducers({
 	category: categoryReducer,
 	subCategory: subCategoryReducer,
 	product: productReducer,
 	subProduct: subProductReducer,
+	token: tokenReducer,
 });
